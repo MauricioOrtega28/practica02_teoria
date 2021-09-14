e58deaed042da27abe226012c1ba9a52a5de5c7c
Mauricio Ortega <mauricio_ortega@usmp.pe>
2021-09-14 17:10:46 -0500

    creacion mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace practica02_teoria.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
